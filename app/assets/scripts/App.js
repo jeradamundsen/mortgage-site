4f80ac591089acb472583ea4c15b9051695da9c9
theSoviet1984 <jerad.amundsen@gmail.com>
2016-12-13 13:57:26 -0600

    build task done

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+import MobileMenu from './modules/MobileMenu';
+import StickyHeader from './modules/StickyHeader';
+import $ from 'jquery';
+
+
+var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
